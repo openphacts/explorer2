16320f4b2d2c37fb686c083a4d4a07e4baa50926
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2013-09-25 15:27:14 +0100

    handle exact and similarity for structure

--- app/assets/javascripts/controllers/compoundStructureIndexController.js
+++ app/assets/javascripts/controllers/compoundStructureIndexController.js
@@ -7,32 +7,40 @@ App.CompoundStructureIndexController = Ember.ObjectController.extend({
  //on what version of ember you are using - TODO upgrade required
  // actions: {
   structureSearch: function(type) {
-    console.log("Set structure search type: " + type);
     this.set('structureSearchType', type);
     var me = this;
     var thisCompound = this.get('content');
     var searcher = new Openphacts.StructureSearch(ldaBaseUrl, appID, appKey);
     var compoundSearcher = new Openphacts.CompoundSearch(ldaBaseUrl, appID, appKey);   
-    var callback=function(success, status, response){
+    var structureSearchCallback=function(success, status, response){
       if (success && response) {
           var results = null;
           if (type == "exact") {
-              results = compoundSearcher.parseExactResponse(response);
-              $.each(results, function(index, pharma) {
-                // fetch each compound and add to records
-                //var structureRecord = App.CompoundStructure.createRecord(pharma);
-                //thisCompound.get('structure').pushObject(pharmaRecord);
+              results = searcher.parseExactResponse(response);
+              $.each(results, function(index, result) {
+                var callback=function(success, status, response){ 
+                  var compound = App.Compound.createRecord(); 
+                  var csURI = response["_about"];
+                  var compoundResult = compoundSearcher.parseCompoundResponse(response); 
+                  compoundResult.csUri = csURI;
+                  compound.setProperties(compoundResult);
+                  thisCompound.get('structure').pushObject(compound)
+                };
+                compoundSearcher.fetchCompound(result, callback);
               });
           } else if (type == "similarity") {
-              results = compoundSearcher.parseSimilarityResponse(response);
-              $.each(results, function(index, pharma) {
-                // fetch each compound and add to records
-                //var structureRecord = App.CompoundStructure.createRecord(pharma);
-                //thisCompound.get('structure').pushObject(pharmaRecord);
+              results = searcher.parseSimilarityResponse(response);
+              $.each(results, function(index, result) {
+                var callback=function(success, status, response){ 
+                  var compound = App.Compound.createRecord(); 
+                  var csURI = response["_about"];
+                  var compoundResult = compoundSearcher.parseCompoundResponse(response); 
+                  compoundResult.csUri = csURI;
+                  compound.setProperties(compoundResult);
+                  thisCompound.get('structure').pushObject(compound)
+                };
+                compoundSearcher.fetchCompound(result, callback);
               });
-              // fetch each compound and add to records
-              //var structureRecord = App.CompoundStructure.createRecord(pharma);
-	          //thisCompound.get('structure').pushObject(pharmaRecord);
           } else if (type == "substructure") {
               results = searcher.parseSubstructureResponse(response);
               $.each(results, function(index, result) {
@@ -40,24 +48,23 @@ App.CompoundStructureIndexController = Ember.ObjectController.extend({
                   var compound = App.Compound.createRecord(); 
                   var csURI = response["_about"];
                   var compoundResult = compoundSearcher.parseCompoundResponse(response); 
-                  //compound.set("id", compoundResult.id);
                   compoundResult.csUri = csURI;
                   compound.setProperties(compoundResult);
                   thisCompound.get('structure').pushObject(compound)
-                }
+                };
                 compoundSearcher.fetchCompound(result, callback);
               });
           }
       }
     };
     if (type == "exact") {
-        searcher.exact(thisCompound.get('smiles'), null, null, null, null, callback);
+        searcher.exact(thisCompound.get('smiles'), null, null, null, null, structureSearchCallback);
     } else if (type == "similarity") {
         // TODO fix start and count at 1 and 100 for the moment
-        searcher.similarity(thisCompound.get('smiles'), null, null, 100, null, null, callback);
+        searcher.similarity(thisCompound.get('smiles'), null, null, 100, null, null, structureSearchCallback);
     } else if (type == "substructure") {
         // TODO fix start and count at 1 and 100 for the moment
-        searcher.substructure(thisCompound.get('smiles'), 100, null, null, callback);
+        searcher.substructure(thisCompound.get('smiles'), 100, null, null, structureSearchCallback);
     }
   }
  // }
