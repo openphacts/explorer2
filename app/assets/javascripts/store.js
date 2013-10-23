4d0c058291633d0a4dcc8b12f76012371970ff85
Ian Dunlop <ianwdunlop@gmail.com>
2013-10-23 12:04:04 +0100

    once we have a pathway find all the compounds for it then find the cw uri via an RSVP trigger and promise

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -63,6 +63,10 @@ App.PathwayAdapter = DS.Adapter.extend({
   find: function(store, type, id) {
     console.log('pathway adapter find');
     var identifier = id;
+    var alert = {};
+    var compoundURLs = [];
+    Ember.RSVP.EventTarget.mixin(alert);
+
     var promise = new Ember.RSVP.Promise(function(resolve, reject){
       var searcher = new Openphacts.PathwaySearch(ldaBaseUrl, appID, appKey);
       var pathwayInfoCallback=function(success, status, response){
@@ -87,12 +91,45 @@ App.PathwayAdapter = DS.Adapter.extend({
             $.each(compoundsResult.metabolites, function(index, metabolite) {
               //load the ids of the pathways, the compound model will lazy load it
               // need to then find the cw id using the mapURL function
-              data['compounds'].push(metabolite);
+              compoundURLs.push(metabolite);
+              //data['compounds'].pushObject(store.find('compound', '83931753-9e3f-4e90-b104-e3bcd0b4d833'));
+              //data['compounds'].pushObject(store.find('compound', '5a814fb1-403a-4dab-8190-b7e6db7b4432'));
+              //data['compounds'].push(metabolite);
             });
-            // all data added so send it back
+            alert.trigger('finished');
         }
+        return data;
       }
       searcher.getCompounds('http://identifiers.org/wikipathways/' + id, null, getCompoundsCallback);
+    });
+    promise.then(function(data) {
+      alert.on("finished", function(event) {
+        console.log('finished');
+        console.log(compoundURLs);
+	    var mapSearcher = new Openphacts.MapSearch(ldaBaseUrl, appID, appKey);
+	    var mapURLCallback = function(success, status, response) {
+		  var constants = new Openphacts.Constants();
+		  if (success && response) {
+              console.log('pathway compounds callback success');
+			  var matchingURL = null;
+		      var urls = mapSearcher.parseMapURLResponse(response);
+              var found = false;
+              //loop through all the identifiers for a compound until we find the cw one
+		      $.each(urls, function(i, url) {
+			    var uri = new URI(url);
+			    if (!found && constants.SRC_CLS_MAPPINGS['http://' + uri.hostname()] == 'conceptWikiValue') {
+                  console.log('compound is ' + url);
+				  data['compounds'].pushObject(store.find('compound', url.split('/').pop()));
+                  found = true;
+				}
+		      });	
+		  }
+	    };
+        $.each(compoundURLs, function(index, URL) {
+	      console.log('url is ' + URL);
+	      mapSearcher.mapURL(URL, null, null, null, mapURLCallback);
+        });
+      });
       return data;
     });
     return promise;
