22853f1f654f92a996aa6e56df4fa8b686bb403a
Ian Dunlop <ianwdunlop@gmail.com>
2013-11-04 16:17:47 +0000

    remove content for previous tree when new tree type is selected

--- app/assets/javascripts/controllers/treesIndexController.js
+++ app/assets/javascripts/controllers/treesIndexController.js
@@ -2,11 +2,25 @@ App.TreesIndexController = Ember.ArrayController.extend({
 
   defaultTree: 'enzyme',
   selectedTree: 'enzyme',
-  treeTypes: ["enzyme", "chembl"],
+  treeTypes: ["enzyme", "chembl", "chebi", "go"],
 
   selectedTreeChanged: function() {
     if(this.get('selectedTree') !== null) {
         console.log('selected tree changed ' + this.get('selectedTree'));
+        this.clear();
+        var me = this;
+	    var searcher = new Openphacts.TreeSearch(ldaBaseUrl, appID, appKey);
+	    var callback = function(success, status, response) {
+		    if (success && response) {
+			    var root = searcher.parseRootNodes(response);
+			    $.each(root, function(index,enzymeResult) {
+				    var enzyme = me.store.createRecord('tree', enzymeResult);
+                    enzyme.set('id', enzymeResult.uri.split('/').pop());
+				    me.addObject(enzyme);				    
+			    });
+			}
+		}
+	    searcher.getRootNodes(this.get('selectedTree'), callback);
     }
   }.observes('selectedTree')
 
