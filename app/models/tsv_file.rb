7add3b78be4e3c87994c4a7bed663344a69f2ffc
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-01-13 12:30:12 +0000

    update the settings and tsv to use api with version number param

--- app/models/tsv_file.rb
+++ app/models/tsv_file.rb
@@ -15,30 +15,30 @@ class TsvFile < ActiveRecord::Base
   def process params
     app_key = AppSettings.config["keys"]["app_key"]
     app_id = AppSettings.config["keys"]["app_id"]
+    app_version = AppSettings.config["tsv"]["api_version"]
     url_path = ''
-    domain = AppSettings.config["tsv"]["url"]
-    logger.info "domain: " + domain.to_s
+    domain = AppSettings.config["tsv"]["tsv_url"]
     path = AppSettings.config["tsv"][params[:request_type] + "_path"]
-    logger.info "tsv file 1"
     url_params = "uri=" + CGI::escape(params[:uri]) + "&_format=tsv" + "&app_id=" + app_id + "&app_key=" + app_key
-    logger.info "tsv file 2"
-    #params[:activity_type] != "" ? url_params += "&activity_type=" + CGI::escape(params[:activity_type]) + "&activity_unit=" + CGI::escape(params[:activity_unit]) + "&" + CGI::escape(params[:activity_value_type]) + "=" + CGI::escape(params[:activity_value]) : ''
-    #params[:assay_organism] != "" ? url_params += "&assay_organism=" + CGI::escape(params[:assay_organism]) : ''
-    #params[:target_organism] != "" ? url_params += "&target_organism=" + CGI::escape(params[:target_organism]) : ''
+    params[:activity_type] != "" ? url_params += "&activity_type=" + CGI::escape(params[:activity_type]) + "&activity_unit=" + CGI::escape(params[:activity_unit]) + "&" + CGI::escape(params[:activity_value_type]) + "=" + CGI::escape(params[:activity_value]) : ''
+    params[:pchembl_value_type] != "" ? url_params += "&" + CGI::escape(params[:pchembl_value_type]) + "=" + CGI::escape(params[:pchembl_value]) : ''
+    params[:assay_organism] != "" ? url_params += "&assay_organism=" + CGI::escape(params[:assay_organism]) : ''
+    params[:target_organism] != "" ? url_params += "&target_organism=" + CGI::escape(params[:target_organism]) : ''
     number_of_pages = (params[:total_count].to_i / 250) + 1
     i = 1
     file = File.new(File.join(Rails.root, "filestore", self.uuid), "w")
-    logger.info "tsv file 3"
     # download the tsv file 250 records at a time
     all_headers = []
     begin
-      CSV.open(file.path, "w", {:col_sep=>"\t", :headers=>true}) do |tab|
+      FasterCSV.open(file.path, "w", {:col_sep=>"\t", :headers=>true}) do |tab|
         while i <= number_of_pages
-          url_path = "#{path}?".concat(url_params).concat("&_page=#{i}&_pageSize=250")
-    logger.info "tsv file 4"
+          if app_version == ""
+            url_path = "#{path}?".concat(url_params).concat("&_page=#{i}&_pageSize=250")
+          else
+            url_path = "/#{app_version}#{path}?".concat(url_params).concat("&_page=#{i}&_pageSize=250")
+          end
           response = Net::HTTP.get(domain, url_path)
-    logger.info "tsv file 5"
-          tab_data = CSV.parse(response, {:col_sep => "\t", :headers => true})
+          tab_data = FasterCSV.parse(response, {:col_sep => "\t", :headers => true})
           # only need the header line from the first response
           if i == 1 
             all_headers = tab_data.headers
@@ -68,23 +68,33 @@ class TsvFile < ActiveRecord::Base
     # no guarantee you will get all the headers so here is a complete list, might change in the future so be aware
     app_key = AppSettings.config["keys"]["app_key"]
     app_id = AppSettings.config["keys"]["app_id"]
+    app_version = AppSettings.config["tsv"]["api_version"]
     url_path = ''
-    all_headers = ["http://www.chemspider.com", "inchi", "inchikey", "smiles", "hba", "hbd", "logp", "psa", "ro5_violations", "http://www.conceptwiki.org", "prefLabel", "http://linkedlifedata.com/resource/drugbank", "biotransformation", "description", "meltingPoint", "proteinBinding", "toxicity", "http://data.kasabi.com/dataset/chembl-rdf", "full_mwt", "molform", "mw_freebase", "rtb", "isPrimaryTopicOf"]
+    all_headers = []
     domain = AppSettings.config["tsv"]["tsv_url"]
     path = "/compound"
     file = File.new(File.join(Rails.root, "filestore", self.uuid), "w")
     first = true
     i = 1
     total = JSON.parse(params[:csids]).size
-    CSV.open(file.path, "w", {:col_sep=>"\t", :headers=>true}) do |tab|
-      tab << all_headers
+    FasterCSV.open(file.path, "w", {:col_sep=>"\t", :headers=>true}) do |tab|
+      #tab << all_headers
       JSON.parse(params[:csids]).each do |csid|
         
-        url_params = "uri=" + CGI::escape("http://rdf.chemspider.com/#{csid}") + "&_format=tsv&app_id=" + app_id + "&app_key=" + app_key
+        url_params = "uri=" + CGI::escape("http://ops.rsc-us.org/#{csid}") + "&_format=tsv&app_id=" + app_id + "&app_key=" + app_key
         begin
-          url_path = "#{path}?".concat(url_params)
+          if app_version == ""
+            url_path = "#{path}?".concat(url_params)
+          else
+            url_path = "/#{app_version}#{path}?".concat(url_params)
+          end
           response = Net::HTTP.get(domain, url_path)
-          tab_data = CSV.parse(response, {:col_sep => "\t", :headers=>true})
+          tab_data = FasterCSV.parse(response, {:col_sep => "\t", :headers=>true})
+          if i == 1 
+            all_headers = tab_data.headers
+            all_headers.delete(nil)
+            tab << all_headers
+          end
           tab_data.each do |row|
             current_row = []
             all_headers.each {|header| current_row << row.values_at(header)}
