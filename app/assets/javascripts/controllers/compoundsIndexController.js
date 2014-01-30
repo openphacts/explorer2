f44d3d00288eae3dc1e03d387beead390dcc6ca4
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-01-30 13:12:39 +0000

    EXPLORER-7 provenance for target

--- app/assets/javascripts/controllers/compoundsIndexController.js
+++ app/assets/javascripts/controllers/compoundsIndexController.js
@@ -1,6 +1,20 @@
 App.CompoundsIndexController = Ember.ObjectController.extend({
 
   queryParams: ['uri'],
-  uri: ''
+  uri: '',
+
+  showProvenance: false,
+
+  actions: {
+  	  enableProvenance: function() {
+    	this.set('showProvenance', true);
+    	console.log("Compund provenance enabled");
+	  },
+
+  	  disableProvenance: function() {
+      	this.set('showProvenance', false);
+    	console.log("Compound provenance disabled");
+  	  }
+  }
 	
 });
