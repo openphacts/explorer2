2d9f29cb9bbf36cdc1bb07d50c454ad30939b431
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-03-04 15:58:02 +0000

    transition to exact structure search after finding drawn compound

--- app/assets/javascripts/controllers/compoundsDrawController.js
+++ app/assets/javascripts/controllers/compoundsDrawController.js
@@ -16,8 +16,6 @@ App.CompoundsDrawController = Ember.ObjectController.extend({
           var me = this;
 	      this.get('controllers.application').set('fetching', true);
           var smiles = document.getElementById('ketcher-iframe').contentWindow.ketcher.getSmiles();
-          //var molfile = document.getElementById('ketcher-iframe').contentWindow.ketcher.getMolfile();
-          //App.Molfile = smiles;
           var structureSearcher = new Openphacts.StructureSearch(ldaBaseUrl, appID, appKey);
           var structureCallback = function(success, status, response) {
               if (success) {
@@ -37,15 +35,13 @@ App.CompoundsDrawController = Ember.ObjectController.extend({
           var me = this;
 	      this.get('controllers.application').set('fetching', true);
           var smiles = document.getElementById('ketcher-iframe').contentWindow.ketcher.getSmiles();
-          //var molfile = document.getElementById('ketcher-iframe').contentWindow.ketcher.getMolfile();
-          //App.Molfile = smiles;
           var structureSearcher = new Openphacts.StructureSearch(ldaBaseUrl, appID, appKey);
           var structureCallback = function(success, status, response) {
               if (success) {
 	            me.get('controllers.application').set('fetching', false);
                 var uri = structureSearcher.parseSmilesToURLResponse(response);
                 // got the uri from the smiles so now fetch the compound
-	            me.transitionToRoute('compounds.index', {queryParams: {'uri': uri}});
+	            me.transitionToRoute('compounds.structure', {queryParams: {'uri': uri, 'type': 'exact'}});
               } else {
                 me.get('controllers.application').set('fetching', false);
 			    App.FlashQueue.pushFlash('error', 'No compound found with this structure');
