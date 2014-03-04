a8a91644851c8e7921343a0b868329916a5c95b8
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-03-04 15:55:40 +0000

    [EXPLORER-41] search by smiles in generic search

--- app/assets/javascripts/controllers/searchController.js
+++ app/assets/javascripts/controllers/searchController.js
@@ -188,14 +188,48 @@ App.SearchController = Ember.ArrayController.extend({
                 // an error in the response, ignore for now
             }
         }; 
+        var structureSearcher = new Openphacts.StructureSearch(ldaBaseUrl, appID, appKey);
+        var structureCallback = function(success, status, response) {
+            if (success) {
+	            me.get('controllers.application').set('fetching', false);
+              var uri = structureSearcher.parseSmilesToURLResponse(response);
+              // got the uri from the smiles so now fetch the compound
+              me.store.find('compound', uri).then(function(compound) {
+                  me.addSearchResult(compound);
+                  //how many pathways & pharmacology for this compound
+                  var pathwaysSearcher = new Openphacts.PathwaySearch(ldaBaseUrl, appID, appKey);
+                  var compoundSearcher = new Openphacts.CompoundSearch(ldaBaseUrl, appID, appKey);
+
+                  var pathwaysCountCallback=function(success, status, response){
+                      if (success && response) {
+                        var count = pathwaysSearcher.parseCountPathwaysByCompoundResponse(response);
+                        compound.set('pathwayRecords', count);
+                      }
+                  };
+
+                  var pharmaCountCallback=function(success, status, response){
+                      if (success && response) {
+                        var count = compoundSearcher.parseCompoundPharmacologyCountResponse(response);
+                        compound.set('pharmacologyRecords', count);
+                      }
+                  };
+                  var compoundURI = compound.get('URI');
+                  compoundSearcher.compoundPharmacologyCount(compoundURI, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, pharmaCountCallback);
+                  pathwaysSearcher.countPathwaysByCompound(compoundURI, null, null, pathwaysCountCallback);
+              });
+
+            } else {
+	          //didn'f find anything, doesn't really matter
+            }
+        }
         //targets
           me.get('controllers.application').set('fetching', true);
           //searching uses branch 3 for compounds and 4 for branches rather than byTag and semantic tag - caused issues due to there being multiple semantic tags for a branch
           searcher.freeText(me.getCurrentQuery(), me.get('numberOfResults'), '3', cwTargetCallback);
           //searcher.byTag(me.getCurrentQuery(), '20', '3', 'a3b5c57e-8ac1-46ac-afef-3347d40c4d37', cwGeneTargetCallback);
           searcher.freeText(me.getCurrentQuery(), me.get('numberOfResults'), '4', cwCompoundCallback);
-          //maybe we can add generic search by smiles?
-          //structureSearcher.smilesToURL(me.getCurrentQuery(), structureSearchCallback);
+		  //smiles for compounds
+          structureSearcher.smilesToURL(me.getCurrentQuery(), structureCallback);
 
    },
 
