e74e4d190010c73ce2d974a6994ff1097ac4134e
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-05-09 14:46:04 +0100

    move hide/show code in to the didScroll function defined in the view. clean up unused code and format

--- app/assets/javascripts/views/compoundsPharmacologyView.js
+++ app/assets/javascripts/views/compoundsPharmacologyView.js
@@ -1,170 +1,61 @@
 App.CompoundsPharmacologyView = Ember.View.extend({
-  didInsertElement: function() {
-      var controller = this.get('controller');
-      $('#assay_organism_box').typeahead({
-        source: function (query, process) {
-            $.getJSON(organismsUrl, { query: query }, function (data) {
-                return process(data);
-            })
-        }
-      });
-      $('#target_organism_box').typeahead({
-        source: function (query, process) {
-            $.getJSON(organismsUrl, { query: query }, function (data) {
-                return process(data);
-            })
-        }
-      });
-    var view = this;
-    $(window).bind("scroll", function() {
-      view.didScroll();
-    });
+    didInsertElement: function() {
+        var controller = this.get('controller');
+        $('#assay_organism_box').typeahead({
+            source: function(query, process) {
+                $.getJSON(organismsUrl, {
+                    query: query
+                }, function(data) {
+                    return process(data);
+                })
+            }
+        });
+        $('#target_organism_box').typeahead({
+            source: function(query, process) {
+                $.getJSON(organismsUrl, {
+                    query: query
+                }, function(data) {
+                    return process(data);
+                })
+            }
+        });
+        var view = this;
+        $(window).bind("scroll", function() {
+            view.didScroll();
+        });
 
-// Hide the extra content initially, using JS so that if JS is disabled, no problemo:
-//$('.read-more-content').addClass('hide')
+    },
 
-// Set up a link to expand the hidden content:
-//.before('<a class="read-more-show" href="#"> more</a>')
-  
-// Set up a link to hide the expanded content.
-//.append(' <a class="read-more-hide" href="#"> less</a>');
-
-// Set up the toggle effect:
-//$('.read-more-show').on('click', function(e) {
-//  $(this).next('.read-more-content').removeClass('hide');
-//  $(this).addClass('hide');
-//  e.preventDefault();
-//  	var s = $("#summary");
-//        s.css({
-//            height: "250px"
-//        });
-//});
-
-//$('.read-more-hide').on('click', function(e) {
-// $(this).parent('.read-more-content').addClass('hide').parent().children('.read-more-show').removeClass('hide');
-//  e.preventDefault();
-//  var s = $("#summary");
-//        s.css({
-//            height: "100%"
-//        });
-//});
-// var toggleSummary = true;
- 
-// $('#toggle-summary').on('click', function() {
-// 	console.log("var " + toggleSummary);
-// 	if (toggleSummary) {
-//		var s = $("#summary");
-//   
-//        s.css({
-//            height: "55px"
-//        });
-//    	$('#structure').hide();
-//			
-//    	var i = $('#compound-image');
-//		i.css({
-//			visibility:"hidden"
-//		});	
-//		toggleSummary = false;
-//		$(this).text(function(i, text){
-//          return "more";
-//      	});
-//      	
-// 	} else {
-//		var s = $("#summary");
-//        s.css({
-//           height: "100%",
-//            width: "100%"
-//        });
-//        $('#structure').show();
-//
-//		var i = $('#compound-image');
-//			i.css({
-//			visibility:"visible"
-//		});
-//		toggleSummary = true;
-//		$(this).text(function(i, text){
-//          return "less";
-//      	});
-// 	}
-// 
-// });
- 
-$(function() {
-	moveScroller();
-});
-  
-//sticky
-function moveScroller() {
-    var move = function() {
+    willDestroyElement: function() {
+        $(window).unbind("scroll");
+    },
+    // If the summary box is scrolling off the page then shrink and fix to top of page.
+    // If scrolled to bottom of page then fetch more results
+    didScroll: function() {
+        var controller = this.get('controller');
         var st = $(window).scrollTop();
         var ot = $("#summary-anchor").offset().top;
         var s = $("#summary");
-        if(st > ot) {
-		controller.set('fixSummaryBox', true);          
-                controller.set('infoHide', true);
-		//    		s.css({
-//            	position: "fixed",
-//            	top: "0px",
-//            	height: "55px"
-//        	});
-//    		$('#structure').hide();
-//			
-//    		var i = $('#compound-image');
-//				i.css({
-//					visibility:"hidden"
-//				});
-//			toggleSummary = false;
-//			$('#toggle-summary').text(function(i, text){
-//    			return "more";
-//    		});
-//		$('#toggle-summary').addClass('hide');
-    		
+        if (st > ot) {
+            controller.set('fixSummaryBox', true);
+            controller.set('infoHide', true);
+
         } else {
-		controller.set('fixSummaryBox', false);
-		controller.set('infoHide', false);
-//			s.css({
-//            	position: "relative",
-//            	top: "",
-//            	height: "100%",
-//            	width: "100%"
-//        	});
-//        	$('#structure').show();
-//
-//			var i = $('#compound-image');
-//				i.css({
-//				visibility:"visible"
-//			});
-//		        $('#toggle-summary').removeClass('hide');
-//			toggleSummary = true;
-//			$('#toggle-summary').text(function(i, text){
-//    			return "less";
-//    		});        
-    	}
-    };
-    $(window).scroll(move);
-    move();
-}
+            controller.set('fixSummaryBox', false);
+            controller.set('infoHide', false);
+        }
+        if (this.isScrolledToBottom() && !this.get('controller').get('fetching')) {
+            disable_scroll();
+            this.get('controller').send('fetchMore');
+        }
+    },
 
-  },
+    isScrolledToBottom: function() {
+        var documentHeight = $(document).height();
+        var windowHeight = $(window).height();
+        var top = $(document).scrollTop();
+        var scrollPercent = (top / (documentHeight - windowHeight)) * 100;
 
-  willDestroyElement: function() {
-    $(window).unbind("scroll");
-  },
-
-  didScroll: function() {
-    if(this.isScrolledToBottom() && !this.get('controller').get('fetching')) {
-      //this.get('controller').set('fetching', true);
-      disable_scroll();
-      this.get('controller').send('fetchMore');
+        return scrollPercent > 99;
     }
-  },
-
-  isScrolledToBottom: function() {
-    var documentHeight = $(document).height();
-    var windowHeight = $(window).height();
-    var top = $(document).scrollTop();
-    var scrollPercent = (top/(documentHeight-windowHeight)) * 100;
-
-    return scrollPercent > 99;
-  }
 });
