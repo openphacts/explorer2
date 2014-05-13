34685e47d14a0abd415bba084e628bb39b5a723d
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-05-13 16:20:26 +0100

    qunit tests for searching

--- test/javascripts/integration/explorerTest.js
+++ test/javascripts/integration/explorerTest.js
@@ -14,3 +14,21 @@ test("Check HTML is returned", function() {
     });
 
 });
+
+test("Check free text search works", function() {
+    expect(6);
+
+    visit("/").then(function() {
+        ok(exists(".navbar-search"), "The navbar was rendered");
+        ok(exists("#search_box"), "Search box was found");
+        ok(exists("#search-button"), "Search button was found");
+    });
+
+    visit("/").then(function() {
+        fillIn("#search_box", "Aspirin");
+    }).then(function() {
+        Ember.run(function() {
+            return click("#search-button");
+        });
+    })
+});
