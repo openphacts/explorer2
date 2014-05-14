7c39ff7a48707495528323cf769a1c5d9f9f120f
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-05-14 16:03:16 +0100

    navigate to compound info page and test

--- test/javascripts/integration/explorerTest.js
+++ test/javascripts/integration/explorerTest.js
@@ -16,7 +16,7 @@ test("Check HTML is returned", function() {
 });
 
 test("Check free text search works", function() {
-    expect(8);
+    expect(5);
 
     visit("/").then(function() {
         ok(exists(".navbar-search"), "The navbar was rendered");
@@ -37,12 +37,17 @@ test("Check free text search works", function() {
             click($('#search-results ul li:first-of-type h4 a')[0]);
         }).then(function() {
             equal(currentRouteName(), "compounds.index", "Clicked on link for Aspirin and transitioned to route");
-        }).then(function() {
-	    click($('#provenance-on-button'));	
-	}).then(function() {
-	    ok(exists("#description-provenance ~ a img"), "Provenance icon shown for description");
-	    ok(exists("h1 a img"), "Provenance icon shown for pref label");
-	    ok(exists("#molform-provenance a img"), "Provenance icon shown for molform");	
-	});
+        });
+    });
+});
+test("Check that compound info is rendered", function() {
+    visit("/compounds?uri=http%3A%2F%2Fwww.conceptwiki.org%2Fconcept%2Fdd758846-1dac-4f0d-a329-06af9a7fa413").then(function() {
+        equal(currentRouteName(), "compounds.index", "Clicked on link for Aspirin and transitioned to route");
+    }).then(function() {
+        click($('#provenance-on-button'));
+    }).then(function() {
+        ok(exists("#description-provenance ~ a img"), "Provenance icon shown for description");
+        ok(exists("h1 a img"), "Provenance icon shown for pref label");
+        ok(exists("#molform-provenance a img"), "Provenance icon shown for molform");
     });
 });
