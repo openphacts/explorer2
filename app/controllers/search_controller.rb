526aea4815bc2dd0887c605238443465ce2dbbbc
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-08-18 13:31:21 +0100

    removed unneeded puts statements from autocompleter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,14 +6,11 @@ class SearchController < ApplicationController
   # and targets for matches. Only matches the start of
   # the prefLabel
   def typeahead
-    puts "inside typeahead"
     results = []
     Rails.cache.fetch('comp_' + params[:query], :expires_in => 6.months) { Compound.where(["label LIKE ?", "%#{params[:query]}%"]).limit(20) }.each do |compound|
-      puts 'compound ' + compound.label
       results.push(compound.label)
     end
     Rails.cache.fetch('tar_' + params[:query], :expires_in => 6.months) { Target.where(["label LIKE ?", "%#{params[:query]}%"]).limit(20) }.each do |target|
-      puts 'target ' + target.label
       results.push(target.label)
     end
     # shuffle the results so that compounds and targets get mixed up since we
