a1dd8be57db9882fefaa78a89abd14905a8ed753
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-09-11 14:12:57 +0100

    compound pathways bs3 classes

--- app/assets/javascripts/controllers/compoundsPathwaysController.js
+++ app/assets/javascripts/controllers/compoundsPathwaysController.js
@@ -1,72 +1,70 @@
 App.CompoundsPathwaysController = Ember.ObjectController.extend({
 
-  needs: ['application'],
+    needs: ['application'],
 
-  queryParams: ['uri'],
+    queryParams: ['uri'],
 
-  uri: '',
+    uri: '',
 
-  page: null,
-  
-  showProvenance: false,
+    page: null,
 
-  currentCount: function() {
-    return this.get('model.pathways.length');
-  }.property('model.pathways.length'),
+    showProvenance: false,
 
-  totalCount: null,
+    currentCount: function() {
+        return this.get('model.pathways.length');
+    }.property('model.pathways.length'),
 
-  notEmpty: function() {
-    return this.get('totalCount') > 0;
-  }.property('totalCount'),
+    totalCount: null,
 
-  fetching: false,
+    notEmpty: function() {
+        return this.get('totalCount') > 0;
+    }.property('totalCount'),
 
-actions: {
-  fetchMore: function() {
-    if (this.get('model.pathways.length') < this.totalCount && this.totalCount > 0 && this.get('controllers.application').get('fetching') === false) {
-        me.get('controllers.application').set('fetching', true);
-    var me = this;
-    var thisCompound = this.get('content');
-    var searcher = new Openphacts.PathwaySearch(ldaBaseUrl, appID, appKey);
-    var pathwaysByCompoundCallback=function(success, status, response){
-      if (success && response) {
-        me.page++;
-        var pathwayResults = searcher.parseByCompoundResponse(response);
-        $.each(pathwayResults, function(index, pathway) {
-            pathwayID = pathway.identifier;
-            //have to find the pathway record and add it, just adding the ID does not work
-            me.get('store').find('pathway', pathwayID).then(function(pathway) {
-              thisCompound.get('pathways').pushObject(pathway);
-            });
-        });
-        me.get('controllers.application').set('fetching', false);
-        enable_scroll();
-      } else {
-        //failed response so scrolling is now allowed
-        me.get('controllers.application').set('fetching', false);
-        enable_scroll();
-      }
-    };
-    searcher.byCompound(thisCompound.get('URI'), null, null, me.page + 1, 50, null, pathwaysByCompoundCallback);
-    } else {
-        enable_scroll();
+    fetching: false,
+
+    actions: {
+        fetchMore: function() {
+            if (this.get('model.pathways.length') < this.totalCount && this.totalCount > 0 && this.get('controllers.application').get('fetching') === false) {
+                me.get('controllers.application').set('fetching', true);
+                var me = this;
+                var thisCompound = this.get('content');
+                var searcher = new Openphacts.PathwaySearch(ldaBaseUrl, appID, appKey);
+                var pathwaysByCompoundCallback = function(success, status, response) {
+                    if (success && response) {
+                        me.page++;
+                        var pathwayResults = searcher.parseByCompoundResponse(response);
+                        $.each(pathwayResults, function(index, pathway) {
+                            pathwayID = pathway.identifier;
+                            //have to find the pathway record and add it, just adding the ID does not work
+                            me.get('store').find('pathway', pathwayID).then(function(pathway) {
+                                thisCompound.get('pathways').pushObject(pathway);
+                            });
+                        });
+                        me.get('controllers.application').set('fetching', false);
+                        enable_scroll();
+                    } else {
+                        //failed response so scrolling is now allowed
+                        me.get('controllers.application').set('fetching', false);
+                        enable_scroll();
+                    }
+                };
+                searcher.byCompound(thisCompound.get('URI'), null, null, me.page + 1, 50, null, pathwaysByCompoundCallback);
+            } else {
+                enable_scroll();
+            }
+        },
+
+        enableProvenance: function() {
+            this.get('showProvenance') === false ? this.set('showProvenance', true) : '';
+        },
+
+        disableProvenance: function() {
+            this.get('showProvenance') === true ? this.set('showProvenance', false) : '';
+        },
+
+        goToTop: function() {
+            window.scrollTo(0, 0);
+        }
     }
-  },
-
-  enableProvenance: function() {
-  	this.set('showProvenance', true);
-   	console.log("Compound pathway provenance enabled " + this.get('showProvenance'));
-  },
-
-  disableProvenance: function() {
-    this.set('showProvenance', false);
-    console.log("Compound pathway provenance disabled " + this.get('showProvenance'));
-  },
-
-  goToTop: function() {
-      window.scrollTo(0,0);
-  }
-}
 
 });
