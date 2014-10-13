45c9b34f210a019d27830ff3ed1eae7599532f8e
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-10-13 13:55:11 +0100

    First version of glmol 3d protein viewer

--- app/assets/javascripts/controllers/targetsIndexController.js
+++ app/assets/javascripts/controllers/targetsIndexController.js
@@ -1,24 +1,58 @@
 App.TargetsIndexController = Ember.ObjectController.extend({
 
-  needs: ['application'],
+    needs: ['application'],
 
-  queryParams: ['uri'],
+    queryParams: ['uri'],
 
-  uri: '',
+    uri: '',
 
-  showProvenance: false,
+    molViewer: null,
 
-  actions: {
-  	  enableProvenance: function() {
-    	this.set('showProvenance', true);
-    	console.log("Target provenance enabled");
-	  },
+    pdbStructure: null,
 
-  	  disableProvenance: function() {
-      	this.set('showProvenance', false);
-    	console.log("Target provenance disabled");
-  	  }
-  }
+    showProvenance: false,
+
+    actions: {
+        enableProvenance: function() {
+            this.set('showProvenance', true);
+            console.log("Target provenance enabled");
+        },
+
+        disableProvenance: function() {
+            this.set('showProvenance', false);
+            console.log("Target provenance disabled");
+        },
+        drawProtein: function() {
+            var me = this;
+            if (me.get('pdbStructure') == null) {
+                var uri = "http://www.pdb.org/pdb/files/" + me.get('content').get('seeAlso')[0].split('/').pop() + ".pdb"
+                $.get(uri, function(ret) {
+                    me.set('pdbStructure', ret);
+                    $("#glmol01_src").val(ret);
+                    me.get('molViewer').loadMolecule();
+                    me.get('molViewer').defineRepresentation = function() {
+                        var all = this.getAllAtoms();
+                        var hetatm = this.removeSolvents(this.getHetatms(all));
+                        this.colorByAtom(all, {});
+                        this.colorByChain(all);
+                        var asu = new THREE.Object3D();
+
+                        this.drawBondsAsStick(asu, hetatm, this.cylinderRadius, this.cylinderRadius);
+                        this.drawBondsAsStick(asu, this.getResiduesById(this.getSidechains(this.getChain(all, ['A'])), [58, 87]), this.cylinderRadius, this.cylinderRadius);
+                        this.drawBondsAsStick(asu, this.getResiduesById(this.getSidechains(this.getChain(all, ['B'])), [63, 92]), this.cylinderRadius, this.cylinderRadius);
+                        this.drawCartoon(asu, all, this.curveWidth, this.thickness);
+
+                        this.drawSymmetryMates2(this.modelGroup, asu, this.protein.biomtMatrices);
+                        this.modelGroup.add(asu);
+                    };;
+                    me.get('molViewer').show();
+                });
+            } else {
+                $("#glmol01_src").val(me.get('pdbStructure'));
+                me.get('molViewer').loadMolecule();
+
+            }
+        }
+    }
 
 });
-    
