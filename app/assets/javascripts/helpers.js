d2d54f9d5888c18080e9a23e956f48dc5e2511c0
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-11-25 13:31:06 +0000

    padding and word break for alerts

--- app/assets/javascripts/helpers.js
+++ app/assets/javascripts/helpers.js
@@ -77,7 +77,7 @@ Ember.Handlebars.registerBoundHelper('textLink', function(link) {
   }
 });
 Ember.Handlebars.registerBoundHelper('getLabelWithTooltip', function(job) {
-  return new Handlebars.SafeString('<td title="' + job.filters + '">' + job.label + '</td>');
+  return new Handlebars.SafeString('<td class="break-all small-padding-right" title="' + job.filters + '">' + job.label + '</td>');
 });
 Ember.Handlebars.registerBoundHelper('progressBar', function(percentage) {
   return new Handlebars.SafeString('<div class="progress progress-striped active no-margin" title="' + percentage + '%"><div class="bar" style="width: ' + percentage + '%;"></div></div>');
