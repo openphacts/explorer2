54d90d13b2410d75d8c73a880b6beff8a4c33b7b
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-12-05 13:38:13 +0000

    remove/comment out lenses route

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Explorer2::Application.routes.draw do
       get :tsv_download
       get :tsv_status
       get :organisms
-      get :lenses
+#      get :lenses
       post :chemspider_tab_separated_file
     end
   end
