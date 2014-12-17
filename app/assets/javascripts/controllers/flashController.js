df69aebd284c7980e9511cfd93c7dcc7dcaad191
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-12-17 14:48:41 +0000

    no need to set the model to [] in the flash controller since it is an ArrayController

--- app/assets/javascripts/controllers/flashController.js
+++ app/assets/javascripts/controllers/flashController.js
@@ -1,4 +1,3 @@
 // Credit to Eric Berry http://coderberry.me/blog/2013/06/20/using-flash-messages-with-emberjs/ for original idea
 App.FlashController = Ember.ArrayController.extend({
-    model: []
 });
