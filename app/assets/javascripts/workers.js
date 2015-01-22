d63ea28fea3a3d052dc2d1bd2b82e99b000c6371
Ian Dunlop <ianwdunlop@gmail.com>
2015-01-22 16:18:12 +0000

    parse the pharma results inside the webworker

--- app/assets/javascripts/workers.js
+++ app/assets/javascripts/workers.js
@@ -16,40 +16,33 @@ onmessage = function(e) {
                 "Accept": "application/json"
             }
         }, function(err, resp, body) {
-            console.log('response');
-        });
-        var pharmaCallback = function(success, status, response) {
-                if (success && response) {
-                    var pharmaResults = searcher.parseCompoundPharmacologyResponse(response);
-                    pharmaResults.forEach(function(result, index, results) {
+            if (err == null && body != null) {
+                var pharmaResponse = JSON.parse(body.toString());
+                var pharmaResults = searcher.parseCompoundPharmacologyResponse(pharmaResponse.result);
+                pharmaResults.forEach(function(result, index, results) {
 
-                    });
-
-                    //write results out to the tsvFile  
-                    //
-                    i++;
-                    if (i <= number_of_pages) {
-                        postMessage({
-                            "status": "processing",
-                            "percent": 100 / number_of_pages * i
-                        });
-                        // target_type & lens not supported yet
-                        searcher.compoundPharmacologyCount(params.uri, params.assay_organism, params.target_organism, params.activity_type, params.activity_value_type === "activity_value" ? params.activity_value : null, params.activity_value_type === "min-activity_value" ? params.activity_value : null, params.activity_value_type === "minEx-activity_value" ? params.activity_value : null, params.activity_value_type === "max-activity_value" ? params.activity_value : null, params.activity_value_type === "maxEx-activity_value" ? params.activity_value : null, params.unit, params.activityRelation, params.pchembl_value_type === "pchembl" ? params.pchembl_value : null, params.pchembl_value_type === "min-pChembl" ? params.pchembl_value : null, params.pchembl_value_type === "minEx-pChembl" ? params.pchembl_value : null, params.pchembl_value_type === "max-pChembl" ? params.pchembl_value : null, params.pchembl_value_type === "maxEx-pChembl" ? params.pchembl_value : null, null, null, pharmaCallback);
-                    } else {
-                        // finished so do something
-                        postMessage({
-                            "status": "complete",
-                            "percent": "100"
-                        });
-                    }
-                } else {
+                });
+                if (i <= number_of_pages) {
                     postMessage({
-                        "status": "failed"
+                        "status": "processing",
+                        "percent": 100 / number_of_pages * i
+                    });
+                } else {
+                    // finished so do something
+                    postMessage({
+                        "status": "complete",
+                        "percent": "100"
                     });
                 }
+                i++;
+            } else {
+                postMessage({
+                    "status": "failed"
+                });
+
             }
-            // target_type and lens not supported yet
-        searcher.compoundPharmacologyCount(params.uri, params.assay_organism, params.target_organism, params.activity_type, params.activity_value_type === "activity_value" ? params.activity_value : null, params.activity_value_type === "min-activity_value" ? params.activity_value : null, params.activity_value_type === "minEx-activity_value" ? params.activity_value : null, params.activity_value_type === "max-activity_value" ? params.activity_value : null, params.activity_value_type === "maxEx-activity_value" ? params.activity_value : null, params.unit, params.activityRelation, params.pchembl_value_type === "pchembl" ? params.pchembl_value : null, params.pchembl_value_type === "min-pChembl" ? params.pchembl_value : null, params.pchembl_value_type === "minEx-pChembl" ? params.pchembl_value : null, params.pchembl_value_type === "max-pChembl" ? params.pchembl_value : null, params.pchembl_value_type === "maxEx-pChembl" ? params.pchembl_value : null, null, null, pharmaCallback);
+            console.log('response');
+        });
     } else if (params.request_type === "target") {
 
     } else if (params.request_type === "structure") {
