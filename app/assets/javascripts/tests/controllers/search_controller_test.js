7600a787dcee652fb0fd581a879995a9c94c0fd7
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2015-02-12 09:40:05 +0000

    test search controller checking/unchecking compounds and targets

---
+++ app/assets/javascripts/tests/controllers/search_controller_test.js
@@ -0,0 +1,35 @@
+emq.globalize();
+App.setupForTesting();
+App.injectTestHelpers();
+setResolver(Ember.DefaultResolver.create({
+    namespace: App
+}));
+
+moduleFor('controller:search', 'Search Controller', {
+	needs: ['controller:application', 'controller:flash']
+});
+test('can see the application controller', function() {
+    expect(2);
+    // get the controller instance
+    var ctrl = this.subject();
+    // check that the compounds index controller can see the application controller
+    appCtrl = ctrl.get('controllers.application');
+    equal(appCtrl.get('alertsAvailable'), false);
+    appCtrl.set('alertsAvailable', true);
+    equal(appCtrl.get('alertsAvailable'), true);
+});
+test('provenance can be enabled and disabled via the actions', function() {
+    expect(6);
+    var ctrl = this.subject();
+    // check the properties before the action is triggered
+    equal(ctrl.get('compoundsChecked'), true);
+    ctrl.send('setCompoundsChecked');
+    equal(ctrl.get('compoundsChecked'), false);
+    ctrl.send('setCompoundsChecked');
+    equal(ctrl.get('compoundsChecked'), true);
+    equal(ctrl.get('targetsChecked'), true);
+    ctrl.send('setTargetsChecked');
+    equal(ctrl.get('targetsChecked'), false);
+    ctrl.send('setTargetsChecked');
+    equal(ctrl.get('targetsChecked'), true);
+});
