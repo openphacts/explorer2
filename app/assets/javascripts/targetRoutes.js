f2fb17ceefed507948395333846fc068525990d9
Ian Dunlop <ianwdunlop@gmail.com>
2015-02-13 15:35:22 +0000

    idea for 404 for model

--- app/assets/javascripts/targetRoutes.js
+++ app/assets/javascripts/targetRoutes.js
@@ -36,6 +36,7 @@ App.TargetsIndexRoute = Ember.Route.extend({
             // TODO need to navigate back somewhere if there is an error, however using window.history.back()
             // might not go back to the correct place because the browser window might have been at a different
             // starting point than the ember app.
+            // Maybe just needs a 404 page eg return this.transitionTo('modelNotFound'); (from ember docs example)
             this.controllerFor('flash').pushObject(this.get('store').createRecord('flashMessage', {
                 type: 'error',
                 message: 'This target is not available, please try a different one.'
