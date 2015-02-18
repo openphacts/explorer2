61ad25bd20f0e9f29f6a8c92bdffeafbbbb879c3
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2015-02-18 16:52:56 +0000

    show buttons to pharma etc in mobile versions if there is info available

--- app/assets/javascripts/models/pathway.js
+++ app/assets/javascripts/models/pathway.js
@@ -1,19 +1,31 @@
 App.Pathway = DS.Model.extend({
-  compound: DS.belongsTo('searchResult'),
-  title: DS.attr('string'),
-  identifier: DS.attr('string'),
-  description: DS.attr('string'),
-  pathwayOntologies: DS.attr(),
-  organism: DS.attr('string'),
-  organismLabel: DS.attr('string'),
-  geneProductLabel: DS.attr('string'),
-  geneProductURI: DS.attr('string'),
-  geneProductCWURI: DS.attr('string'),
-  about: DS.attr('string'),
-  parts: DS.attr(),
-  compounds: DS.hasMany('compound', { async: true }),
-  targets: DS.hasMany('target', { async: true }),
-  revision: DS.attr('string'),
-  URI: DS.attr('string'),
-  wikipathwaysProvenance: DS.attr()
+    compound: DS.belongsTo('searchResult'),
+    title: DS.attr('string'),
+    identifier: DS.attr('string'),
+    description: DS.attr('string'),
+    pathwayOntologies: DS.attr(),
+    organism: DS.attr('string'),
+    organismLabel: DS.attr('string'),
+    geneProductLabel: DS.attr('string'),
+    geneProductURI: DS.attr('string'),
+    geneProductCWURI: DS.attr('string'),
+    about: DS.attr('string'),
+    parts: DS.attr(),
+    compounds: DS.hasMany('compound', {
+        async: true
+    }),
+    compoundRecords: DS.attr('number'),
+    compoundInfoAvailable: function() {
+        return (this.get('compoundRecords') !== null && this.get('compoundRecords') >= 0);
+    }.property('compoundRecords'),
+
+    hasCompounds: function() {
+        return this.get('compoundRecords') > 0;
+    }.property('compoundRecords'),
+    targets: DS.hasMany('target', {
+        async: true
+    }),
+    revision: DS.attr('string'),
+    URI: DS.attr('string'),
+    wikipathwaysProvenance: DS.attr()
 });
