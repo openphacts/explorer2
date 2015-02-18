bea5f4d645296d9fa91fa522196282962eac5f72
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-02-18 16:27:46 +0000

    avoid debug output

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 sed -i "s,url *:.*,url: $API_URL," /explorer2/config/app_settings.yml
 sed -i "s,app_id *:.*,app_id: $API_APP_ID," /explorer2/config/app_settings.yml
 sed -i "s,app_key *:.*,app_key : $API_APP_KEY," /explorer2/config/app_settings.yml
-cat /explorer2/config/app_settings.yml
+#cat /explorer2/config/app_settings.yml
 
 exec "$@"
 
