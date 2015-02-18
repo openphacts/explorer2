94a3f85ed2af11981ffa0c73f1f1136cf98568d2
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-02-18 16:38:55 +0000

    Docker: start delayed_job worker from entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,8 @@ sed -i "s,app_id *:.*,app_id: $API_APP_ID," /explorer2/config/app_settings.yml
 sed -i "s,app_key *:.*,app_key : $API_APP_KEY," /explorer2/config/app_settings.yml
 #cat /explorer2/config/app_settings.yml
 
+/explorer2/script/delayed_job start
+
 exec "$@"
 
 
